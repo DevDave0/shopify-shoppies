28c9664756668718e406e3f721cd1ddc34c6653f
devdave0 <dbchung24@gmail.com>
2021-01-09 17:39:51 -0500

    test omdb api key

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import './App.css';
 import React from 'react';
+const exampleKey = "http://www.omdbapi.com/?i=tt3896198&apikey=4ccaac08"
+// const key = "http://www.omdbapi.com/?t=[]apikey=4ccaac08"
+// api key is : 4ccaac08
+
 
 class App extends React.Component {
 
   state = {
+    results:[],
+    nominations:[],
+    title: '',
+    year: '',
 
   }
 
   componentDidMount() {
+    fetch(exampleKey)
+    .then(resp => resp.json())
+    .then(movie => {
+      this.setState({
+        title: movie.Title,
+        year: movie.Year
+      })
+    })
   }
 
   render(){
     return (
       <div>
-        <p>Test</p>
+        <h1>Movie Title: {this.state.title}</h1>
+        <h3>Year released: {this.state.year}</h3>
       </div>
     );
   }
