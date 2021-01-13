be10f871b979f93b69a85092ecaf56e1241324d1
devdave0 <dbchung24@gmail.com>
2021-01-13 17:11:42 -0500

    add search box

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieList from './components/MovieList';
 import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
 
 
 const App = () => {
@@ -26,8 +27,9 @@ const App = () => {
 
   return (
     <div className='container-fluid movie-app'>
-      <div className='row'>
+      <div className='row d-flex align-items-center mt-4 mb-4'>
         <MovieListHeading heading="Movies" />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
       </div>
       <div className='row'>
         <MovieList movies={movies} />
